dabb6b99ea19198881e2bd4adfe057393a6dcf04
Ricardo Guedes <ricardo@amazoniasistemas.com.br>
2020-08-19 07:48:56 -0400

    challenge finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(o => o.id === id);
+
+  if (index < 0)
+    return response.status(400).send();
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+
+  return response.status(200).json(repositories[index]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(o => o.id === id);
+
+  if (index < 0)
+    return response.status(400).send();
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(o => o.id === id);
+  if (index < 0)
+    return response.status(400).send();
+
+  repositories[index].likes++;
+
+  return response.status(200).json(repositories[index]);
 });
 
 module.exports = app;
